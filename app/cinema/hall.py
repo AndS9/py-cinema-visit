57000ea3bf46549a5c5290c11620a47497e78523
Andrii Shukalo <shukaload@gmail.com>
2026-02-08 22:20:57 +0200

    Change parameter name in CinemaHall.movie_session method.

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -7,10 +7,10 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customer: list[Customer],
+    def movie_session(self, movie_name: str, customers: list[Customer],
                       cleaning_staff: Cleaner) -> None:
         print(f'\"{movie_name}\" started in hall number {self.number}.')
-        for customer in customer:
+        for customer in customers:
             customer.watch_movie(movie_name)
         print(f'\"{movie_name}\" ended.')
 
